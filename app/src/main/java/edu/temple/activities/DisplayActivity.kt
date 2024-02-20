a869552e13fa731edc189bd5a8cc419fb1b66579
sanHemet <20hhemet@gmail.com>
2024-02-20 11:58:56 -0500

    Added functionality to change size of text in a different activity

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,7 +1,9 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class DisplayActivity : AppCompatActivity() {
@@ -9,11 +11,17 @@ class DisplayActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
+        val selectedTextSize = intent.getFloatExtra("textSize", 12.0f)
+
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
-
+            textSize = selectedTextSize
         }
 
 
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
     }
 }
